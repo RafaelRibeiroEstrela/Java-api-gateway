48760ee6182c060271771627932c1d75b3fa1b5e
Rafael Ribeiro Estrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2026-01-06 15:33:37 -0400

    Ajustando groupid e pacote

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package estrela.r.rafael.apigateway;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
